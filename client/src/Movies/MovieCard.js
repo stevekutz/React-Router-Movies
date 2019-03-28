784182afff8afa75a23dae875ea76db3f68cb26b
stevekutz <stkutz@gmail.com>
2019-03-27 21:20:14 -0500

    building MovieCard, accepting destructured props

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,55 @@
 import React from 'react';
 
+
+
+/*
+<div className="movie-card">
+  <h2>{title}</h2>
+  <div className="movie-director">
+    Director: <em>{director}</em>
+  </div>
+  <div className="movie-metascore">
+    Metascore: <strong>{metascore}</strong>
+  </div>
+  <h3>Actors</h3>
+
+  {stars.map(star => (
+    <div key={star} className="movie-star">
+      {star}
+    </div>
+  ))}
+</div>
+
+ */
+
+
+
 const MovieCard = props => {
-  return;
+  console.log("MovieCard props ", props);
+
+  const {id, title, director, metascore, stars} = props.movie;
+  return (
+    <div className="movie-card">
+      <h2>{title} id {id}</h2>
+      <div className="movie-director">
+        Director: <em>{director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
+  )
 };
+
+
+
+
 
 export default MovieCard;
